f265dab1948a27f0bd13c1cea892235351db4fef
Shim <rm88760@fiap.com.br>
2021-03-17 09:04:28 -0300

    adição ex 1

---
+++ 1.py
@@ -0,0 +1,8 @@
+numero_1=int(input("entre com o primeiro numero:"))
+numero_2=int(input("entre com o segundo numero:"))
+numero_3=int(input("entre com o terceiro numero:"))
+
+soma= numero_1 + numero_2 + numero_3
+quadrado= soma * soma
+
+print("o quadrado da soma é:" ,quadrado)
